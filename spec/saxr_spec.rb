2283a56ddb5f5e081b3e590e89b23d10213063c9
Brian Murphy-Dye <brian@murphydye.com>
2015-09-08 19:40:35 -0600

    Add simplest example.

--- spec/saxr_spec.rb
+++ spec/saxr_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
-describe Saxr do
-  it 'has a version number' do
-    expect(Saxr::VERSION).not_to be nil
+class Example < Saxr::SuperSax
+  def self.default_directory
+    Pathname(__FILE__).expand_path.dirname
+  end
+
+  def self.default_filename
+    'example.xml'
+  end
+end
+
+describe 'Saxr::SuperSax' do
+  let(:filename) { 'example.xml' }
+
+
+  context 'parses' do
+    it 'real good' do
+      sax = Example.parse
+    end
   end
 end
